cf8d288ac07c3446fb88e16dd1736a9fdd739176
Samuel Levy <samuellevy@Samuels-iMac.local>
2018-08-06 10:35:02 -0300

    initial commit

---
+++ wp-content/plugins/ams/ams.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: AMS - Ads Management System by Nokengo
+Plugin URI: http://nokengo.com/
+Description: An advertising management system for WordPress.
+Version: 0.0.1
+Author: Samuel Levy
+Author http://nokengo.com/
+License: Education
+Text Domain: ams
+*/
+
+/*
+ *      Copyright 2012 Aluno da Escola WordPress <email@exemplo.org>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+class Ams{
+    function init(){
+        echo('Teste');
+    }
+}
+
+$ams = new Ams();
